a4daf61f13e97e20cac8f3649ebd92b6e0698070
quaxsze <service@plum.eu.com>
2021-06-12 18:34:17 +0200

    config fix

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     MONGODB_HOST = os.environ.get('MONGODB_HOST')
-    ELASTICSEARCH_URL = os.environ.get(
-        'ELASTICSEARCH_URL') or 'http://localhost:9200'
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 20
